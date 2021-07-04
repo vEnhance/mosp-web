2820da11f3c2943423aa07da063ec121c4c15b57
Evan Chen <evan@evanchen.cc>
2021-07-04 11:17:30 -0500

    Image things

---
+++ typescripts/src/images.ts
@@ -0,0 +1,13 @@
+$(() =>
+  $('#container img').each(function(_, el) {
+    const url = $(el).attr('src')!;
+    if (url.indexOf('-sm') !== -1) {
+      const new_url = url.replace('-sm', '');
+      window.setTimeout(() => {
+        $("<img />").attr('src', new_url).on('load', () => {
+          $(el).attr('src', new_url);
+        });
+      }, 10);
+    }
+  });
+});
--- typescripts/src/main.ts
+++ typescripts/src/main.ts
@@ -10,6 +10,7 @@ require.config({
 require([
   "sweetalert2",
   "sha",
+  "images",
   "checkname",
   "grader"
 ]);
