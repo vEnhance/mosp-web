c2f55ac971aa9d33bfd2027294429a3f4596988c
Evan Chen <evan@evanchen.cc>
2021-07-04 11:34:03 -0500

    Images typescript and all

--- typescripts/src/images.ts
+++ typescripts/src/images.ts
@@ -1,13 +1,11 @@
-$(() =>
+$(() => {
   $('#container img').each(function(_, el) {
     const url = $(el).attr('src')!;
-    if (url.indexOf('-sm') !== -1) {
-      const new_url = url.replace('-sm', '');
-      window.setTimeout(() => {
-        $("<img />").attr('src', new_url).on('load', () => {
-          $(el).attr('src', new_url);
-        });
-      }, 10);
+    if (url.indexOf('-sm.') !== -1) {
+      const new_url = url.replace('-sm.', '.');
+      $("<img />").attr('src', new_url).on('load', () => {
+        $(el).attr('src', new_url);
+      });
     }
   });
 });
