24771bbd1f09bf349a925149f3ad71c7e6855079
Evan Chen <evan@evanchen.cc>
2021-07-15 10:30:13 -0700

    Add django auth user

--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import Q, Max
 from django.urls import reverse_lazy
@@ -302,6 +303,9 @@ class Token(models.Model):
 			primary_key = True,
 			default = uuid.uuid4,
 			editable = False)
+	user = models.OneToOneField(User,
+			on_delete = models.SET_NULL,
+			null = True, blank = True)
 	name = models.CharField(max_length = 128,
 			help_text = "Who are you?")
 	reduced_name = models.CharField(max_length = 128,
