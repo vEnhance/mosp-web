c21a845913073bc5f1c75e10175dbc6501a3803b
Evan Chen <evan@evanchen.cc>
2021-07-23 20:32:41 -0700

    jesus mysql

--- mospweb/settings.py
+++ mospweb/settings.py
@@ -134,7 +134,7 @@ if os.getenv("DATABASE_NAME"):
 			'PASSWORD' : os.getenv("DATABASE_PASSWORD"),
 			'HOST' : os.getenv("DATABASE_HOST"),
 			'PORT' : os.getenv("DATABASE_PORT", '3306'),
-			'OPTIONS' : {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
+			'OPTIONS' : {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'", 'charset': 'utf8mb4'},
 		},
 	}
 else:
