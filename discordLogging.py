d5100100242cfbaa4bdfd706c6cecf77e8b0e25e
Evan Chen <evan@evanchen.cc>
2021-08-14 22:16:42 -0700

    Upgrade discord logger
    borrowed from OTISWEB

--- discordLogging.py
+++ discordLogging.py
@@ -1,6 +1,16 @@
 import logging
-import requests
+import os
+import pprint
 import socket
+from collections import OrderedDict
+from typing import Any, Dict
+
+import requests
+from django.http import HttpRequest
+from dotenv import load_dotenv
+
+load_dotenv()
+WEBHOOK_DEFAULT_URL = os.getenv('WEBHOOK_URL')
 
 COLORS = {
 	"default": 2040357,
@@ -15,7 +25,7 @@ COLORS = {
 EMOJIS = {
 	"default": ":loudspeaker:",
 	"error": ":x:",
-	"critical": ":fire:",
+	"critical": ":skull_crossbones:",
 	"warning": ":warning:",
 	"info": ":bell:",
 	"verbose": ":mega:",
@@ -24,56 +34,99 @@ EMOJIS = {
 }
 
 class DiscordHandler(logging.Handler):
-	def __init__(self, url = None):
-		super().__init__()
-		self.url = url
-
-	def emit(self, r: logging.LogRecord):
-		level = r.levelname.lower().strip()
+	def emit(self, record: logging.LogRecord):
+		level = record.levelname.lower().strip()
 		emoji = EMOJIS.get(level, ':question:')
 		color = COLORS.get(level, 0xaaaaaa)
-		if '\n' not in r.message:
-			i = None
-			title = f'{emoji} {r.message[:200]}'
+
+		try:
+			user = record.request.user.first_name + ' ' + record.request.user.last_name # type: ignore
+		except AttributeError:
+			user = 'anonymous'
+		s = str(getattr(record, 'status_code', ''))
+		if s:
+			s = '**' + s + '**'
 		else:
-			i = r.message.index('\n')
-			title = f"{emoji} {r.message[:i]}"
+			s = 'None'
 
 		fields = [
-				{ 'name': 'Line', 'value': '`' + str(r.lineno) + '`', 'inline': True, },
-				{ 'name': 'File', 'value': '`' + str(r.filename) + '`', 'inline': True, },
-				{ 'name': 'Scope', 'value': '`' + r.name + '`', 'inline': True, }
+				{ 'name': 'Status', 'value': s, 'inline': True, },
+				{ 'name': 'Level', 'value': record.levelname.title(), 'inline': True, },
+				{ 'name': 'Scope', 'value': f"`{record.name}`", 'inline': True, },
+				{ 'name': 'Module', 'value': f"`{record.module}`", 'inline': True, },
+				{ 'name': 'User', 'value': user, 'inline': True, },
+				{ 'name': 'Filename', 'value': f"{record.lineno}:`{record.filename}`", 'inline': True, },
 				]
 
-		status_code = getattr(r, 'status_code', None)
+		description_parts = OrderedDict()
+
+		# if the message is short (< 1 line), we set it as the title
+		if '\n' not in record.message:
+			title = f'{emoji} {record.message[:200]}'
+		# otherwise, set the first line as title and include the rest in description
+		else:
+			i = record.message.index('\n')
+			title = f"{emoji} {record.message[:i]}"
+			description_parts[':green_heart: MESSAGE :green_heart:'] =  "```" + record.message[i+1:] + "```"
+
+		# if exc_text nonempty, add that to description
+		if record.exc_text is not None:
+			# always truncate r.exc_text to at most 600 chars since it's fking long
+			if len(record.exc_text) > 600:
+				blob = record.exc_text[:300] + '\n...\n' + record.exc_text[-300:]
+			else:
+				blob = record.exc_text
+			description_parts[':yellow_heart: EXCEPTION :yellow_heart:'] =  "```" + blob + "```"
+
+		# if request data is there, include that too
+		if hasattr(record, 'request'):
+			request: HttpRequest = getattr(record, 'request')
+			s = ''
+			s += f'> **Method** {request.method}\n'
+			s += f'> **Path** `{request.path}`\n'
+			s += f'> **Content Type** {request.content_type}\n'
+			s += f'> **Agent** {request.headers.get("User-Agent", "Unknown")}\n'
+			if request.user.is_authenticated:
+				s += f'> **User** {getattr(request.user, "username", "wtf")}\n'
+			if request.method == 'POST':
+				# redact the token for evan's personal api
+				d: Dict[str, Any] = {}
+				for k,v in request.POST.items():
+					if k == 'token' or k == 'password':
+						d[k] = '<redacted>'
+					else:
+						d[k] = v
+				s += r'POST data' + '\n'
+				s += r'```' + '\n'
+				pp = pprint.PrettyPrinter(indent = 2)
+				s += pp.pformat(d)
+				s += r'```'
+			if request.FILES is not None and len(request.FILES) > 0:
+				s += f'Files included\n'
+				for name, fileobj in request.FILES.items():
+					s += f'> `{name}` ({fileobj.size} bytes, { fileobj.content_type })\n'
+			description_parts[':blue_heart: REQUEST :blue_heart:'] = s
+
 		embed = {
 					'title': title,
 					'color': color,
 					'fields': fields
 				}
 
-		description = ''
-		if r.exc_text is not None:
-			msg = r.exc_text
-			if len(msg) > 800:
-				msg = msg[:300] + '\n...\n' + msg[-500:]
-			description = (f'HTTP {status_code}' \
-					if status_code is not None else '') + \
-					f'```\n{msg}\n```'
-		if i is not None:
-			msg = r.message[i+1:]
-			if len(msg) > 800:
-				msg = msg[:300] + '\n...\n' + msg[-500:]
-			description += f"\n```{msg}```\n"
-		description = description.strip()
-		if len(description) > 0:
-			embed['description']  = description
+		desc = ''
+		no_worries = sum(len(_) for _ in description_parts.values()) <= 1800
+		for k,v in description_parts.items():
+			if len(v) < 600 or no_worries:
+				desc += k + '\n' + v.strip() + '\n'
+		if desc:
+			embed['description'] = desc
 
 		data = {
 				'username': socket.gethostname(),
 				'embeds': [embed],
 				}
 
-		if self.url is not None:
-			response = requests.post(self.url, json = data)
-			return response
+		url = os.getenv(f'WEBHOOK_URL_{level.upper()}', WEBHOOK_DEFAULT_URL)
+
+		if url is not None:
+			print(requests.post(url, json = data))
