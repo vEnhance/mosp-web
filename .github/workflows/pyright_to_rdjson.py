80e6189e3759f81727354f728785b4c85926fdde
Evan Chen <evan@evanchen.cc>
2021-08-16 03:35:29 -0700

    hm I think the line indexing is off

--- .github/workflows/pyright_to_rdjson.py
+++ .github/workflows/pyright_to_rdjson.py
@@ -2,45 +2,50 @@
 
 import json
 import sys
-
 from typing import Any, Dict, TextIO
 
 
 def pyright_to_rdjson(jsonin: TextIO):
-    pyright = json.load(jsonin)
+	pyright = json.load(jsonin)
 
-    if "generalDiagnostics" not in pyright:
-        raise RuntimeError("This doesn't look like pyright json")
+	if "generalDiagnostics" not in pyright:
+		raise RuntimeError("This doesn't look like pyright json")
 
-    rdjson: Dict[str, Any] = {
-        "source": {"name": "pyright", "url": "https://github.com/Microsoft/pyright"},
-        "severity": "WARNING",
-        "diagnostics": [],
-    }
+	rdjson: Dict[str, Any] = {
+		"source": {
+		"name": "pyright",
+		"url": "https://github.com/Microsoft/pyright"
+		},
+		"severity": "WARNING",
+		"diagnostics": [],
+	}
 
-    for d in pyright["generalDiagnostics"]:
-        rdjson["diagnostics"].append(
-            {
-                "message": f"{d['message']} ({d['rule']})",
-                "severity": d["severity"].upper(),
-                "location": {
-                    "path": d["file"],
-                    "range": {
-                        "start": {
-                            "line": d["range"]["start"]["line"],
-                            "column": d["range"]["start"]["character"],
-                        },
-                        "end": {
-                            "line": d["range"]["end"]["line"],
-                            "column": d["range"]["end"]["character"],
-                        },
-                    },
-                },
-            }
-        )
+	for d in pyright["generalDiagnostics"]:
+		rdjson["diagnostics"].append(
+			{
+			"message": f"{d['message']} ({d['rule']})",
+			"severity": d["severity"].upper(),
+			"location":
+			{
+			"path": d["file"],
+			"range":
+			{
+			"start":
+			{
+			"line": d["range"]["start"]["line"] + 1,
+			"column": d["range"]["start"]["character"],
+			},
+			"end": {
+			"line": d["range"]["end"]["line"] + 1,
+			"column": d["range"]["end"]["character"],
+			},
+			},
+			},
+			}
+		)
 
-    return json.dumps(rdjson)
+	return json.dumps(rdjson)
 
 
 if __name__ == "__main__":
-    print(pyright_to_rdjson(sys.stdin))
+	print(pyright_to_rdjson(sys.stdin))
--- garbage.py
+++ garbage.py
@@ -1,1 +1,2 @@
+print('hi')
 print('3' + None)
