83516e0c77c725d047419a50b5cccc702853cfdd
Evan Chen <evan@evanchen.cc>
2021-09-08 10:04:57 -0400

    fix(stubs): monkey patch needed

--- mospweb/settings.py
+++ mospweb/settings.py
@@ -16,7 +16,10 @@ import platform
 import sys
 from pathlib import Path
 
+import django_stubs_ext
 from dotenv import load_dotenv
+
+django_stubs_ext.monkeypatch()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
