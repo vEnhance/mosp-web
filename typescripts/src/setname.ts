421e131cfb8fd5fa645e2f03479ab433d1be7da6
Evan Chen <evan@evanchen.cc>
2023-03-07 14:54:12 -0800

    fix: make name function work

--- typescripts/src/setname.ts
+++ typescripts/src/setname.ts
@@ -1,24 +1,30 @@
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
+
+declare var solver_name: string;
 
 function setName(name: string) {
-  $.post('/ajax', {
-    action : 'set_name',
-    name : name,
-  }, (result) => {
-    if (result.success == 1) {
-      $("#tokenname").html(name);
+  $.post(
+    "/ajax",
+    {
+      action: "set_name",
+      name: name,
+    },
+    (result) => {
+      if (result.success == 1) {
+        $("#tokenname").html(name);
+      }
     }
-  });
+  );
 }
 
 function getName() {
   Swal.fire({
     title: "Hello hello hey",
     text: "What should I call you?",
-    input: 'text',
-    icon: 'info',
-    confirmButtonText: 'Set name',
-  }).then((result) =>  {
+    input: "text",
+    icon: "info",
+    confirmButtonText: "Set name",
+  }).then((result) => {
     if (result.isConfirmed) {
       setName(result.value);
     }
@@ -26,7 +32,8 @@ function getName() {
 }
 
 $(() => {
-  $("#setname").on('click', () => {
+  $(".name").html(solver_name);
+  $("#setname").on("click", () => {
     getName();
   });
 });
